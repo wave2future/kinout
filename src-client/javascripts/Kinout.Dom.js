cc84c36e37cb221650b54ebd8b3542da8849e707
@soyjavi <javi.jimenez.villar@gmail.com>
2011-10-12 08:43:04 +0200

    Kinout own DOMhandler

---
+++ src-client/javascripts/Kinout.Dom.js
@@ -0,0 +1,116 @@
+/** 
+ * Description or Responsability 
+ * 
+ * @namespace KINOUT
+ * @class Dom
+ *
+ * @author Javier Jimenez Villar <javi@tapquo.com> || @soyjavi
+ */
+
+KINOUT.Dom = (function(knt, undefined) {
+
+    /**
+     * ?
+     *
+     * @method addClass
+     *
+     * params {string} Selector
+     * params {string} Class name to append to container
+     */
+    var addClass = function(selector, css_class) {
+        var elements = _getElements(selector);
+        for (index in elements) {
+            var element = elements[index];
+            var element_hasClass = _hasClass(element, css_class);
+
+            if (!element_hasClass) {
+                element.className += ' ' + css_class;
+            }
+        }
+    };
+
+    /**
+     * ?
+     *
+     * @method removeClass
+     *
+     * params {string} Selector
+     * params {string} Class name to append to container
+     */
+    function removeClass(selector, css_class) {
+        var elements = _getElements(selector);
+        for (index in elements) {
+            var element = elements[index];
+            var element_hasClass = _hasClass(element, css_class);
+
+            if (element_hasClass) {
+                var reg = new RegExp('(\\s|^)' + css_class + '(\\s|$)');
+                element.className = element.className.replace(reg, ' ');
+            }
+        }
+    };
+
+    /**
+     * ?
+     *
+     * @method append
+     *
+     * params {string} Selector
+     * params {string} Markup to append
+     */
+    var append = function(selector, markup) {
+
+    };
+
+    /**
+     * ?
+     *
+     * @method prepend
+     *
+     * params {string} Container selector
+     * params {string} Markup to prepend
+     */
+    var prepend = function(selector, markup) {
+        var element = get(selector);
+        if (element) {
+            element.innerHTML = markup + element.innerHTML;
+        }
+    };
+
+    /**
+     * ?
+     *
+     * @method get
+     *
+     * params {string} Container selector
+     */
+    var get = function(selector) {
+        return document.querySelector(selector);
+    };
+
+    var _getElements = function(selector) {
+        var elements = [];
+
+        try {
+            var elements = Array.prototype.slice.call(document.querySelectorAll(selector));
+        } catch (e) {
+            console.error(e);
+        }
+        return elements;
+    };
+
+    function _hasClass(element, css_class) {
+        console.error(element.className);
+        return element.className.match(new RegExp('(\\s|^)' + css_class + '(\\s|$)'));
+    }
+
+
+    return {
+        addClass: addClass,
+        removeClass: removeClass,
+        append: append,
+        prepend: prepend,
+        get: get
+    };
+
+})(KINOUT);
